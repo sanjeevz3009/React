bf748b2cae4bbcac3bbd659595455ed48933f872
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-04 23:55:13 +0100

    Destructuring props and state.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -9,16 +9,16 @@ import Counter from './components/counter';
 function App() {
   return (
     <div className="App">
-    <Counter />
+    {/* <Counter /> */}
     {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman"></Greet>
         <p>This is children props</p>
       <Greet name="Clark" heroName="Superman"></Greet>
-        <button>Action</button>
+        <button>Action</button> */}
       <Greet name="Diana" heroName="Wonder women"></Greet>
       <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/>
-      <Welcome name="Diana" heroName="Wonder women"/> */}
+      {/* <Welcome name="Clark" heroName="Superman"/> */}
+      {/* <Welcome name="Diana" heroName="Wonder women"/> */}
       {/* <Hello></Hello> */}
     </div>
   );
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,12 +4,13 @@ import React from 'react';
 //     return <h1>Hello Sanj</h1>
 // }
 
-const Greet = (props) => {
-    console.log(props);
+const Greet = ({name, heroName}) => {
+    // console.log(props);
+    // const {name, heroName} = props;
     return (
     <div>
-        <h1>Hello {props.name} a.k.a {props.heroName}</h1>
-        {props.children};
+        <h1>Hello {name} a.k.a {heroName}</h1>
+        {/* {props.children} */}
     </div>
     )
 }
