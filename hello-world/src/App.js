92f4f7cf7356d5d3c6fa78b5853fac09cf87cbed
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-04 19:43:37 +0100

    State.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/hello';
+import Message from './components/message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman"></Greet>
+    <Message />
+      {/* <Greet name="Bruce" heroName="Batman"></Greet>
         <p>This is children props</p>
       <Greet name="Clark" heroName="Superman"></Greet>
         <button>Action</button>
       <Greet name="Diana" heroName="Wonder women"></Greet>
       <Welcome name="Bruce" heroName="Batman"/>
       <Welcome name="Clark" heroName="Superman"/>
-      <Welcome name="Diana" heroName="Wonder women"/>
+      <Welcome name="Diana" heroName="Wonder women"/> */}
       {/* <Hello></Hello> */}
     </div>
   );
