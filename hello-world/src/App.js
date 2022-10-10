59499c5e8e4fe333d6265166d3979bfd16ee3b83
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-10 15:08:11 +0100

    Event Handling.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,27 +1,34 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/hello';
 import Message from './components/message';
 import Counter from './components/counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
-function App() {
-  return (
-    <div className="App">
-    {/* <Counter /> */}
-    {/* <Message /> */}
-      {/* <Greet name="Bruce" heroName="Batman"></Greet>
-        <p>This is children props</p>
-      <Greet name="Clark" heroName="Superman"></Greet>
-        <button>Action</button> */}
-      <Greet name="Diana" heroName="Wonder women"></Greet>
-      <Welcome name="Bruce" heroName="Batman"/>
-      {/* <Welcome name="Clark" heroName="Superman"/> */}
-      {/* <Welcome name="Diana" heroName="Wonder women"/> */}
-      {/* <Hello></Hello> */}
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+      <FunctionClick></FunctionClick>
+      <ClassClick></ClassClick>
+      {/* <Counter /> */}
+      {/* <Message /> */}
+        {/* <Greet name="Bruce" heroName="Batman"></Greet>
+          <p>This is children props</p>
+        <Greet name="Clark" heroName="Superman"></Greet>
+          <button>Action</button> */}
+        {/* <Greet name="Diana" heroName="Wonder women"></Greet> */}
+        {/* <Welcome name="Bruce" heroName="Batman"/> */}
+        {/* <Welcome name="Clark" heroName="Superman"/> */}
+        {/* <Welcome name="Diana" heroName="Wonder women"/> */}
+        {/* <Hello></Hello> */}
+      </div>
+    );
+  }
 }
 
 export default App;
