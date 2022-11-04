956defcf7bd43e18542b99b138db911df3929552
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-11-04 15:20:20 +0000

    Component lifecycle methods.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -17,12 +17,14 @@ import Inline from './components/Inline'
 import './components/appStyles.css'
 import styles from './components/appStyles.module.css'
 import Form from './components/Form'
+import LifecycleA from './components/LifecycleA'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-      <Form></Form>
+      <LifecycleA></LifecycleA>
+      {/* <Form></Form> */}
       {/* <h1 className='error'>Error</h1> */}
       {/* <h1 className={styles.success}>Success</h1> */}
       {/* <Inline></Inline> */}
---
+++ hello-world/src/components/LifecycleA.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import LifecycleB from './LifecycleB'
+
+class LifecycleA extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: 'Sanjeev'
+    }
+    console.log('LifecycleA constructor')
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('LifecycleA getDerivedStateFromProps')
+    return null
+  }
+
+  componentDidMount() {
+    console.log('LifecycleA componentDidMount')
+  }
+
+  render() {
+    console.log('LifecycleA render')
+    return (
+      <div>
+        <div>Lifecycle A</div>
+        <LifecycleB></LifecycleB>
+      </div>
+    )
+  }
+}
+
+export default LifecycleA
