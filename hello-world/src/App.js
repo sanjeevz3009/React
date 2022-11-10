b079936f866450c1d9cbc5d8e59f2c7f14ae9477
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-11-10 16:14:32 +0000

    Portals and Error Boundaries.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -25,12 +25,31 @@ import ParentComp from './components/ParentComp'
 import RefsDemo from './components/RefsDemo'
 import FocusInput from './components/FocusInput'
 import FRParentInput from './components/FRParentInput'
+import PortalDemo from './components/PortalDemo'
+import Hero from './components/Hero'
+import ErrorBoundary from './components/ErrorBoundary'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-      <FRParentInput></FRParentInput>
+
+      <ErrorBoundary>
+        <Hero heroName='Batman'></Hero>
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Hero heroName='Superman'></Hero>
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Hero heroName='Joker'></Hero>
+      </ErrorBoundary>
+
+
+      
+      {/* <PortalDemo></PortalDemo> */}
+      {/* <FRParentInput></FRParentInput> */}
       {/* <FocusInput></FocusInput> */}
       {/* <RefsDemo></RefsDemo> */}
       {/* <ParentComp></ParentComp> */}
